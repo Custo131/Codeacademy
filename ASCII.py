edcaa32e0decaeb9f6a2b6420d2439c446b42ebd
Custo131 <128417017+Custo131@users.noreply.github.com>
2024-08-18 16:25:57 +0200

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,11 @@
+#My name is Gustavo
+# I ENJOY EMINEM 
+
+print("K    K  T T T T T")
+print("K   K       T")  
+print("K  K        T")   
+print("K K         T")   
+print("K  K        T")   
+print("K   K       T")
+print("K    K      T") 
+ 
